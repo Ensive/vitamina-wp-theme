2740ca042a7faf5aa0bac55e0c7f7f4dafae2570
Anton Goncharuk <antonge92@gmail.com>
2017-09-23 19:01:47 +0300

    Initial commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antonge
+ * Date: 9/23/17
+ * Time: 5:59 PM
+ */
---
+++ header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antonge
+ * Date: 9/23/17
+ * Time: 5:58 PM
+ */
