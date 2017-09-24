183b195d4232a7965ec8ec133f3b359b33de7ebe
Anton Goncharuk <antonge92@gmail.com>
2017-09-25 02:27:26 +0300

    Add mixin (media). Add some utils. Apply CSS properties order. Use media mixin. Improve responsive design. Hide logo for mobile.

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,5 @@
 <div class="sidebar">
+
   <nav class="sidebar__nav">
     <a class="sidebar__link" href="#">#Birou</a>
     <a class="sidebar__link is-active" href="#">#Proiecte</a>
@@ -10,7 +11,7 @@
     <a class="sidebar__link" href="#">#Toate</a>
   </nav>
 
-  <a href="<?php bloginfo('url'); ?>" class="sidebar__logo" role="banner">
+  <a href="<?php bloginfo('url'); ?>" class="sidebar__logo u-desktop-only" role="banner">
     <img src="<?php echo IMAGES . 'vitamina-logo.jpg' ?>" alt="Vitamina logo" />
   </a>
   <?php if ( is_home() ) { ?>
