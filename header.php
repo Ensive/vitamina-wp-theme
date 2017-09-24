ffa650243c4dcd7ec201be99ef9318461d888732
Anton Goncharuk <antonge92@gmail.com>
2017-09-24 03:30:20 +0300

    Update padding/margin for actions in project item. Revert back the change with sidebar link color. Add bunch of fav/app icons to the root folder. Update header. Small updates.

--- header.php
+++ header.php
@@ -6,13 +6,20 @@
   <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
   <meta name="format-detection" content="telephone=no">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!--vitaminaro app-->
-
   <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
 
   <!--<link rel="apple-touch-icon" href="apple-touch-icon.png">-->
   <!-- TODO: Place favicon.ico in the root directory -->
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri() . '/apple-touch-icon.png' ?>">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri() . '/favicon-32x32.png' ?>">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri() . '/favicon-16x16.png' ?>">
+  <link rel="manifest" href="<?php echo get_stylesheet_directory_uri() . '/manifest.json' ?>">
+  <link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri() . '/safari-pinned-tab.svg' ?>" color="#696969">
+  <meta name="apple-mobile-web-app-title" content="Vitamin A.">
+  <meta name="application-name" content="Vitamin A.">
+  <meta name="theme-color" content="#eee8cb">
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" async></script>
   <?php wp_head(); ?>
